5516dec11793e771753b4a763bcd9bd04075aa73
dantruonghtno1 <dantruonghtno1@gmail.com>
2022-07-28 16:45:16 +0700

    push bert + cnn model

--- Huggingface-example/README.md
+++ Huggingface-example/README.md
@@ -3,6 +3,7 @@
 - [x] : Text classification with trainer hugginggface 
 - [x] : Text classification with accelerate
 - [x] : Text classification using suicao's solution 
+- [x] : Text classification using bert + cnn
 - [ ] : Token classification
 - [ ] : Question Answering
 - [ ] : Text generate 
@@ -47,3 +48,22 @@ We concatenated the last 4 hidden representations of the [CLS] token on top and 
     --output_dir $(pwd)/output \ 
     --report_to "wandb"
 ```
+
+
+## Text classification using Bert + CNN 
+```
+    python text_clf_with_bert_cnn.py \
+    --model_name_or_path xlm-roberta-large \
+    --dataset_name your_dataset  \
+    --do_train \
+    --do_eval \
+    --max_seq_length 256 \
+    --per_device_train_batch_size 32 \
+    --save_steps 5000 \
+    --save_total_limit  2 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3 \
+    --output_dir $(pwd)/output \ 
+    --report_to "wandb"
+```
+
---
+++ Huggingface-example/text_clf_with_bert_cnn.py
@@ -0,0 +1,382 @@
+
+import logging
+import os
+import random
+import sys
+from dataclasses import dataclass, field
+from typing import Optional
+import torch
+from torch import nn
+import datasets
+import numpy as np
+from datasets import load_dataset, load_metric
+
+import transformers
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    EvalPrediction,
+    HfArgumentParser,
+    PretrainedConfig,
+    Trainer,
+    TrainingArguments,
+    default_data_collator,
+    set_seed,
+    BertPreTrainedModel, 
+    AutoModel
+)
+from typing import List, Optional, Tuple, Union
+from packaging import version
+from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+import torch 
+from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers import BertPreTrainedModel, BertModel, AutoConfig, AutoTokenizer
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.utils import check_min_version
+from transformers.utils.versions import require_version
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class DataTrainingArguments:
+    dataset_name: Optional[str] = field(
+        default=None, metadata={"help": "Dataset load from Hunggingface hub "}
+    )
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": "maximun length of sequence tokens of tokenization"
+        }
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached preprocessed datasets or not."}
+    )
+    pad_to_max_length: bool = field(
+        default=True,
+        metadata={
+            "help": "padding to max sequece length or not?"
+        },
+    )
+    max_train_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number of training examples to this "
+            "value if set."
+        },
+    )
+    max_eval_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
+            "value if set."
+        },
+    )
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: str = field(
+        metadata={"help": "model load from huggingface hub or your local"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "model load from hf hub for your local"}
+    )
+
+# Ref: https://github.com/huggingface/transformers/issues/1827 
+# and: https://github.com/huggingface/transformers/blob/main/src/transformers/models/bert/modeling_bert.py#L1378
+
+
+class ModifyModel(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+        self.bert = BertModel(config)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.filter_sizes=[3, 4, 5]
+        self.num_filters=[100, 100, 100]
+        self.conv1d_list = nn.ModuleList([
+            nn.Conv1d(in_channels=config.hidden_size,
+                      out_channels=self.num_filters[i],
+                      kernel_size=self.filter_sizes[i])
+            for i in range(len(self.filter_sizes))
+        ])
+        
+        self.fc = nn.Linear(np.sum(self.num_filters), config.num_labels)
+        # Initialize weights and apply final processing
+        self.post_init()
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
+        r"""
+            apply suicao method to modify the model: 
+            1. concat last 4 cls token 
+            2. dropout
+            3. linear layer : 4*hidden_size -> num_labels
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=True,
+            return_dict=return_dict,
+        )
+        pooled_output = outputs[1]
+        hidden_states = outputs[2][-1]
+        
+#         print('hidden states : ', hidden_states.shape)
+#         hidden_states: [batch, max_seq_len, hidden_size]
+        hidden_states = hidden_states.permute(0,2,1)
+        x_conv_list = [F.relu(conv1d(hidden_states)) for conv1d in self.conv1d_list]
+        x_pool_list =  [F.max_pool1d(x_conv, kernel_size=x_conv.shape[2])
+            for x_conv in x_conv_list]
+        
+        x_fc = torch.cat([x_pool.squeeze(dim=2) for x_pool in x_pool_list],
+                         dim=1)
+        
+        logits = self.fc(self.dropout(x_fc))
+#         cls_tokens = torch.cat((outputs[2][-1][:,0, ...],outputs[2][-2][:,0, ...], outputs[2][-3][:,0, ...], outputs[2][-4][:,0, ...]),-1)
+#         pooled_output = self.dropout(cls_tokens)
+#         logits = self.classifier(cls_tokens)
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+        return loss, logits
+
+
+def main():
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    # logging config
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    log_level = training_args.get_process_log_level()
+    logger.setLevel(log_level)
+    datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+    logger.info(f"Training/evaluation parameters {training_args}")
+
+    # check last checkpoint if checkpoint exist.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
+    # random seed to initialize model
+    set_seed(training_args.seed)
+
+
+    if data_args.dataset_name is not None:
+        # Downloading and loading a dataset from the hub.
+        raw_datasets = load_dataset(
+            data_args.dataset_name,
+        )
+
+    # Labels
+    # https://huggingface.co/docs/datasets/package_reference/main_classes.html#datasets.Dataset.unique
+    label_list = raw_datasets["train"].unique("labels")
+    label_list.sort()  # Let's sort it for determinism
+    num_labels = len(label_list)
+
+    # Load pretrained model and tokenizer fron hub or your local
+    config = AutoConfig.from_pretrained(
+        model_args.model_name_or_path,
+        num_labels=num_labels,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        use_fast=True,
+    )
+    model = ModifyModel.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+    )
+    # check padding dataset 
+    if data_args.pad_to_max_length:
+        padding = "max_length"
+    else:
+        padding = False
+
+    if data_args.max_seq_length > tokenizer.model_max_length:
+        logger.warning(
+            f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the"
+            f"model ({tokenizer.model_max_length}). Using max_seq_length={tokenizer.model_max_length}."
+        )
+    max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
+
+    def preprocess_function(examples):
+        # Tokenize the texts
+        result = tokenizer(examples["text"], padding=padding, max_length=max_seq_length, truncation=True)
+        result["labels"] = examples["labels"]
+        return result
+
+    with training_args.main_process_first(desc="dataset map pre-processing"):
+        raw_datasets = raw_datasets.map(
+            preprocess_function,
+            batched=True,
+            load_from_cache_file=not data_args.overwrite_cache,
+            desc="Running tokenizer on dataset",
+        )
+    if training_args.do_train:
+        if "train" not in raw_datasets:
+            raise ValueError("--do_train requires a train dataset")
+        train_dataset = raw_datasets["train"]
+        if data_args.max_train_samples is not None:
+            max_train_samples = min(len(train_dataset), data_args.max_train_samples)
+            train_dataset = train_dataset.select(range(max_train_samples))
+
+    if training_args.do_eval:
+        if "valid" not in raw_datasets:
+            raise ValueError("--do_eval requires a validation dataset")
+        eval_dataset = raw_datasets["valid"]
+        if data_args.max_eval_samples is not None:
+            max_eval_samples = min(len(eval_dataset), data_args.max_eval_samples)
+            eval_dataset = eval_dataset.select(range(max_eval_samples))
+    # Log random training data sample
+    if training_args.do_train:
+        for index in random.sample(range(len(train_dataset)), 3):
+            logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
+
+    # metric
+    metric = load_metric("f1")
+    from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+
+    def compute_metrics(pred):
+        labels = pred.label_ids
+        preds = pred.predictions.argmax(-1)
+        precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='micro')
+        acc = accuracy_score(labels, preds)
+        return {
+            'accuracy': acc,
+            'f1': f1,
+            'precision': precision,
+            'recall': recall
+        }
+    # Data collator will default to DataCollatorWithPadding when the tokenizer is passed to Trainer, so we change it if
+    # we already did the padding.
+    if data_args.pad_to_max_length:
+        data_collator = default_data_collator
+    elif training_args.fp16:
+        data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
+    else:
+        data_collator = None
+
+    # Initialize our Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        compute_metrics=compute_metrics,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
+
+    # Training
+    if training_args.do_train:
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        metrics = train_result.metrics
+        max_train_samples = (
+            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
+        )
+        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+    # Evaluation
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        eval_datasets = [eval_dataset]
+        for eval_dataset in eval_datasets:
+            metrics = trainer.evaluate(eval_dataset=eval_dataset)
+            max_eval_samples = (
+                data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+            )
+            metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+            trainer.log_metrics("eval", metrics)
+
+    if training_args.push_to_hub:
+        trainer.push_to_hub()
+    else:
+        trainer.create_model_card()
+        
+def _mp_fn(index):
+    # For xla_spawn (TPUs)
+    main()
+
+
+if __name__ == "__main__":
+    main()
